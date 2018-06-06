c11f43f87ef524ff7bd05be3281ca07e8b67ebe4
Emily Fortuna <efortuna@google.com>
2018-06-06 16:28:50 -0700

    Wrote basics of looking at a PR

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
-// Bunch o'hard-coded stuff that will get updated from the response of the JSON/GraphQT API.
+// Bunch o'hard-coded stuff that will get updated from the response of the JSON/GraphQL API.
 // General info about a repo (example): https://api.github.com/repos/efortuna/memechat
 // PRs (example): https://api.github.com/repos/flutter/flutter/pulls
 var profilePic = 'https://avatars0.githubusercontent.com/u/2112792?v=4';
 var username = 'efortuna';
-var defaultRepoInfo = {
+var repoInfo = {
   'name': 'A Repository',
   'stargazers_count': '3',
   'forks_count': '5',
 };
+// Example URL.
+var diffUrl =
+    'https://patch-diff.githubusercontent.com/raw/flutter/flutter/pull/18193.diff';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: "Dude, Where's My Pull Request?",
-      theme: new ThemeData(
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
@@ -31,28 +36,21 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-            title: new Row(
-          children: <Widget>[
-            new Icon(FontAwesomeIcons.github),
-            new Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: new Text("Dude, Where's My Pull Request?"),
-            ),
-          ],
-        )),
-        body: new Center(
-          child: new Column(
+    return Scaffold(
+        appBar: AppBar(
+            leading: Icon(FontAwesomeIcons.github),
+            title: Text("Dude, Where's My Pull Request?")),
+        body: Center(
+          child: Column(
             children: <Widget>[
-              new CircleAvatar(
-                  backgroundImage: new NetworkImage(profilePic), radius: 50.0),
-              new Text(username,
+              CircleAvatar(
+                  backgroundImage: NetworkImage(profilePic), radius: 50.0),
+              Text(username,
                   style:
                       TextStyle(fontWeight: FontWeight.bold, fontSize: 32.0)),
-              new Expanded(
-                child: new ListView(
-                    children: new List.generate(15, (i) => new RepoWidget())),
+              Expanded(
+                child:
+                    ListView(children: List.generate(15, (i) => RepoWidget())),
               ),
             ],
           ),
@@ -63,13 +61,92 @@ class MyHomePage extends StatelessWidget {
 class RepoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new ListTile(
-        title: new Text(defaultRepoInfo['name']),
+    return ListTile(
+        title: Text(repoInfo['name']),
+        // Might want to change how we navigate to the PRs. Maybe a FAB somewhere?
+        onTap: () async {
+          var result =
+              await http.get(diffUrl).then((response) => response.body);
+          return Navigator.push(context,
+              MaterialPageRoute(builder: (context) => ReviewPage(result)));
+        },
         trailing: new Row(children: [
           Icon(Icons.star),
-          Text(defaultRepoInfo['stargazers_count']),
+          Text(repoInfo['stargazers_count']),
           Icon(FontAwesomeIcons.codeBranch),
-          Text(defaultRepoInfo['forks_count'])
+          Text(repoInfo['forks_count'])
         ]));
+  }
+}
+
+class ReviewPage extends StatelessWidget {
+  final String prDiff;
+
+  // Yes, this assumes only one review per repo. We could add a button for
+  // "next review" or something if we wanted.
+  ReviewPage(this.prDiff);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Review Pull Request')),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: SingleChildScrollView(
+                    child: Padding(
+              padding: const EdgeInsets.all(20.0),
+                      // TODO(efortuna): Make this prettier.
+                      // Perhaps https://github.com/toufikzitouni/flutter-bidirectional_scrollview_plugin
+              child: RichText(
+                  softWrap: false, text: TextSpan(children: styledCode())),
+            ))),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton.icon(
+                      onPressed: () => closePR(context),
+                      icon: Icon(Icons.close),
+                      label: Text('Close Request'),
+                      color: Colors.red),
+                ),
+                RaisedButton.icon(
+                    onPressed: () => acceptPR(context),
+                    icon: Icon(Icons.check),
+                    label: Text('Accept Pull Request'),
+                    color: Colors.green),
+              ],
+            ),
+          ],
+        ));
+  }
+
+  List<TextSpan> styledCode() {
+    var lines = <TextSpan>[];
+    for (var line in LineSplitter.split(prDiff)) {
+      var color = Colors.black;
+      if (line.startsWith('+')) {
+        color = Colors.green;
+      } else if (line.startsWith('-')) {
+        color = Colors.red;
+      }
+      lines.add(TextSpan(
+          text: line + '\n',
+          style: TextStyle(color: color, fontFamily: 'monospace')));
+    }
+    return lines;
+  }
+
+  acceptPR(BuildContext context) {
+    // TODO(efortuna): Implement.
+
+    Navigator.pop(context);
+  }
+
+  closePR(BuildContext context) {
+    // TODO(efortuna): Implement.
+
+    Navigator.pop(context);
   }
 }
