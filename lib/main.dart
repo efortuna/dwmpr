87b9b0614faa9bc7e1b24388a654e88ce0dccc41
Emily Fortuna <efortuna@google.com>
2018-07-09 18:12:25 -0700

    Moving code around, cleanup of main file

--- lib/main.dart
+++ lib/main.dart
@@ -3,31 +3,16 @@
 // found in the LICENSE file.
 
 import 'package:dwmpr/github/graphql.dart';
-import 'package:dwmpr/github/pullrequest.dart';
-import 'package:dwmpr/state.dart';
-import 'package:dwmpr/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'dart:math' as math;
-import 'dart:convert';
-import 'package:bidirectional_scroll_view/bidirectional_scroll_view.dart';
-import 'package:dwmpr/github/token.dart';
 
 import 'package:dwmpr/github/graphql.dart' as graphql;
 import 'package:dwmpr/github/user.dart';
 
-// Bunch o'hard-coded stuff that will get updated from the response of the JSON/GraphQL API.
-// General info about a repo (example): https://api.github.com/repos/efortuna/memechat
-// PRs (example): https://api.github.com/repos/flutter/flutter/pulls
-
-// Example URL.
-// You want to start with listing the PRs:
-// https://api.github.com/repos/efortuna/test_commits/pulls
-// We'll pass in the issue_url (for commenting) as well as the diff_url
-// (for displaying the diff)
-// The logic for getting that will be in GraphQL.
-final enableReactions = 'application/vnd.github.squirrel-girl-preview+json';
+import 'review_code.dart';
+import 'state.dart';
+import 'utils.dart';
 
 // Github brand colors
 // https://gist.github.com/christopheranderton/4c88326ab6a5604acc29
@@ -97,7 +82,7 @@ class UserBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+    return Column(children: [
       CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl), radius: 50.0),
       Text(
         user.login,
@@ -145,171 +130,8 @@ class RepoWidget extends StatelessWidget {
   }
 }
 
-class FancyFab extends StatefulWidget {
-  @override
-  createState() => FancyFabState();
-}
-
-class FancyFabState extends State<FancyFab> with TickerProviderStateMixin {
-  AnimationController _controller;
-
-  static const List<IconData> icons = const [
-    Icons.check,
-    Icons.do_not_disturb,
-    Icons.thumb_up,
-    Icons.thumb_down,
-    Icons.favorite,
-    FontAwesomeIcons.question,
-    Icons.cake,
-  ];
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500),
-    );
-  }
-
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: List<Widget>.generate(icons.length, (int index) {
-        return ScaleTransition(
-          scale: CurvedAnimation(parent: _controller, curve: Curves.easeOut),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: FloatingActionButton(
-              heroTag: null,
-              backgroundColor: Theme.of(context).cardColor,
-              mini: true,
-              child: Icon(icons[index], color: Theme.of(context).accentColor),
-              onPressed: () {
-                if (icons[index] == Icons.check) {
-                  acceptPR(context);
-                } else if (icons[index] == Icons.do_not_disturb) {
-                  closePR(context);
-                } else {
-                  addEmoji(context, icons[index]);
-                }
-              },
-            ),
-          ),
-        );
-      }).toList()
-        ..add(
-          FloatingActionButton(
-            child: AnimatedBuilder(
-              animation: _controller,
-              builder: (BuildContext context, Widget child) {
-                return Transform.rotate(
-                  angle: _controller.value * math.pi,
-                  child:
-                      Icon(_controller.isDismissed ? Icons.code : Icons.close),
-                );
-              },
-            ),
-            onPressed: () {
-              if (_controller.isDismissed) {
-                _controller.forward();
-              } else {
-                _controller.reverse();
-              }
-            },
-          ),
-        ),
-    );
-  }
-
-  acceptPR(BuildContext context) {
-    final reviewUrl = PullRequestDetails.of(context).url;
-    http.put('$reviewUrl/merge',
-        headers: {'Authorization': 'token $token'}).then(respondToRequest);
-  }
-
-  closePR(BuildContext context) {
-    final reviewUrl = PullRequestDetails.of(context).url;
-    http
-        .patch(reviewUrl,
-            headers: {'Authorization': 'token $token'},
-            body: '{"state": "closed"}')
-        .then(respondToRequest);
-  }
-
-  void addEmoji(BuildContext context, IconData icon) {
-    final String issueUrl = PullRequestDetails.of(context).url;
-    String reaction = 'heart';
-    if (icon == Icons.thumb_up) {
-      reaction = '+1';
-    } else if (icon == Icons.thumb_down) {
-      reaction = '-1';
-    } else if (icon == Icons.cake) {
-      reaction = 'hooray';
-    } else if (icon == FontAwesomeIcons.question) {
-      reaction = 'confused';
-    }
-    http
-        .post('${issueUrl}/reactions',
-            headers: {
-              'Authorization': 'token $token',
-              'Accept': enableReactions
-            },
-            body: '{"content": "$reaction"}')
-        .then(respondToRequest);
-  }
-
-  respondToRequest(http.Response response) {
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      Navigator.pop(context);
-    } else {
-      print(
-          'Problem completing request: ${response.statusCode} ${response.body}');
-    }
-  }
-}
-
-class ReviewPage extends StatelessWidget {
-  final String prDiff;
-
-  // Yes, this assumes only one review per repo. We could add a button for
-  // "next review" or something if we wanted.
-  ReviewPage(this.prDiff);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Review Pull Request')),
-      body: BidirectionalScrollViewPlugin(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child:
-              RichText(softWrap: false, text: TextSpan(children: styledCode())),
-        ),
-      ),
-      floatingActionButton: FancyFab(),
-    );
-  }
-
-  List<TextSpan> styledCode() {
-    var lines = <TextSpan>[];
-    for (var line in LineSplitter.split(prDiff)) {
-      var color = Colors.black;
-      if (line.startsWith('+')) {
-        color = Colors.green;
-      } else if (line.startsWith('-')) {
-        color = Colors.red;
-      }
-      lines.add(TextSpan(
-          text: line + '\n',
-          style: TextStyle(color: color, fontFamily: 'monospace')));
-    }
-    return lines;
-  }
-}
-
 /// Handles fetching and caching user data for a FutureBuilder
-Function(BuildContext, AsyncSnapshot<User>) _fetchUser(Widget child) {
+_fetchUser(Widget child) {
   return (context, snapshot) {
     if (snapshot.connectionState == ConnectionState.done)
       return UserDetails(user: snapshot.data, child: child);
@@ -319,7 +141,7 @@ Function(BuildContext, AsyncSnapshot<User>) _fetchUser(Widget child) {
 }
 
 /// Handles fetching and caching pull request data for a FutureBuilder
-Function(BuildContext, AsyncSnapshot<List<PullRequest>>) _fetchPullRequests(
+_fetchPullRequests(
     Widget child) {
   return (context, snapshot) {
     if (snapshot.connectionState == ConnectionState.done) {
---
+++ lib/review_code.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+import 'dart:convert';
+import 'package:bidirectional_scroll_view/bidirectional_scroll_view.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:math' as math;
+import 'package:dwmpr/github/token.dart';
+
+import 'state.dart';
+
+final enableReactions = 'application/vnd.github.squirrel-girl-preview+json';
+
+class ReviewPage extends StatelessWidget {
+  final String prDiff;
+
+  // Yes, this assumes only one review per repo. We could add a button for
+  // "next review" or something if we wanted.
+  ReviewPage(this.prDiff);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Review Pull Request')),
+      body: BidirectionalScrollViewPlugin(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child:
+          RichText(softWrap: false, text: TextSpan(children: styledCode())),
+        ),
+      ),
+      floatingActionButton: FancyFab(),
+    );
+  }
+
+  List<TextSpan> styledCode() {
+    var lines = <TextSpan>[];
+    for (var line in LineSplitter.split(prDiff)) {
+      var color = Colors.black;
+      if (line.startsWith('+')) {
+        color = Colors.green;
+      } else if (line.startsWith('-')) {
+        color = Colors.red;
+      }
+      lines.add(TextSpan(
+          text: line + '\n',
+          style: TextStyle(color: color, fontFamily: 'monospace')));
+    }
+    return lines;
+  }
+}
+
+class FancyFab extends StatefulWidget {
+  @override
+  createState() => FancyFabState();
+}
+
+class FancyFabState extends State<FancyFab> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  static const List<IconData> icons = const [
+    Icons.check,
+    Icons.do_not_disturb,
+    Icons.thumb_up,
+    Icons.thumb_down,
+    Icons.favorite,
+    FontAwesomeIcons.question,
+    Icons.cake,
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        children: List<Widget>.generate(icons.length, (int index) {
+      return ScaleTransition(
+        scale: CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FloatingActionButton(
+            heroTag: null,
+            backgroundColor: Theme.of(context).cardColor,
+            mini: true,
+            child: Icon(icons[index], color: Theme.of(context).accentColor),
+            onPressed: () {
+              if (icons[index] == Icons.check) {
+                acceptPR(context);
+              } else if (icons[index] == Icons.do_not_disturb) {
+                closePR(context);
+              } else {
+                addEmoji(context, icons[index]);
+              }
+            },
+          ),
+        ),
+      );
+    }).toList()
+      ..add(
+        FloatingActionButton(
+          child: AnimatedBuilder(
+            animation: _controller,
+            builder: (BuildContext context, Widget child) {
+              return Transform.rotate(
+                angle: _controller.value * math.pi,
+                child:
+                Icon(_controller.isDismissed ? Icons.code : Icons.close),
+              );
+            },
+          ),
+          onPressed: () {
+            if (_controller.isDismissed) {
+              _controller.forward();
+            } else {
+              _controller.reverse();
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  acceptPR(BuildContext context) {
+    final reviewUrl = PullRequestDetails.of(context).url;
+    http.put('$reviewUrl/merge',
+        headers: {'Authorization': 'token $token'}).then(respondToRequest);
+  }
+
+  closePR(BuildContext context) {
+    final reviewUrl = PullRequestDetails.of(context).url;
+    http
+        .patch(reviewUrl,
+        headers: {'Authorization': 'token $token'},
+        body: '{"state": "closed"}')
+        .then(respondToRequest);
+  }
+
+  void addEmoji(BuildContext context, IconData icon) {
+    final String issueUrl = PullRequestDetails.of(context).url;
+    String reaction = 'heart';
+    if (icon == Icons.thumb_up) {
+      reaction = '+1';
+    } else if (icon == Icons.thumb_down) {
+      reaction = '-1';
+    } else if (icon == Icons.cake) {
+      reaction = 'hooray';
+    } else if (icon == FontAwesomeIcons.question) {
+      reaction = 'confused';
+    }
+    http
+        .post('${issueUrl}/reactions',
+        headers: {
+          'Authorization': 'token $token',
+          'Accept': enableReactions
+        },
+        body: '{"content": "$reaction"}')
+        .then(respondToRequest);
+  }
+
+  respondToRequest(http.Response response) {
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      Navigator.pop(context);
+    } else {
+      print(
+          'Problem completing request: ${response.statusCode} ${response.body}');
+    }
+  }
+}
