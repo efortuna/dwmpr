1234ca283426922e5b557b15183ae97465d2a667
Emily Fortuna <efortuna@google.com>
2018-07-09 18:32:29 -0700

    added start state (more coming for sure)

---
+++ lib/steps/start/main.dart
@@ -0,0 +1,41 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart' as http;
+
+// Github brand colors
+// https://gist.github.com/christopheranderton/4c88326ab6a5604acc29
+final Color githubBlue = Color(0xff4078c0);
+final Color githubGrey = Color(0xff333000);
+final Color githubPurple = Color(0xff6e5494);
+
+void main() => runApp(MyApp());
+
+// Root widget of the app
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Dude, Where's My Pull Request?",
+      theme: ThemeData(
+        primaryColor: githubGrey,
+        accentColor: githubBlue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            leading: Icon(FontAwesomeIcons.github),
+            title: Text("Dude, Where's My Pull Request?")),
+        body: Center());
+  }
+}
